93981092da22318c783c64c66a13c9df99dc6155
Dae Sang Yoon <ryanyoon3@gmail.com>
2021-11-20 20:28:11 -0800

    query

--- backend/googleimages_api.py
+++ backend/googleimages_api.py
@@ -6,7 +6,7 @@ PROJECT_CX = 'ac62a5a5d4b56d944'
 def get_image(query):
     gis = GoogleImagesSearch(API_KEY,  PROJECT_CX)
     _search_params = {
-        'q': 'lebron james',
+        'q': query,
         'num': 1
     }
     gis.search(search_params=_search_params)
